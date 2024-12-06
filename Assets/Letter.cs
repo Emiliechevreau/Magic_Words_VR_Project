9fe4b2e1d74e883f80b45a118989ce890151cbdf
Emiliechevreau <emilie.chevreau@edu.devinci.fr>
2024-12-06 22:18:33 +0100

    Initial commit

---
+++ Assets/Letter.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Letter : MonoBehaviour
+{
+    public string letter; // La lettre associée à ce cube
+
+}
